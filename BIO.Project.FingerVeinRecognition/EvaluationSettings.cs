a3abb904cce9d03c2e35157dd01b53e946be71ee
fridex <fridolin.pokorny@gmail.com>
2015-12-07 20:13:11 -0800

    Exchange names of algs based on progress

--- BIO.Project.FingerVeinRecognition/EvaluationSettings.cs
+++ BIO.Project.FingerVeinRecognition/EvaluationSettings.cs
@@ -15,10 +15,10 @@ namespace BIO.Project.FingerVeinRecognition
     > {
 
         public EvaluationSettings() {
-            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents1("Alg1").createBlock());
-            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents2("Alg2").createBlock());
-            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents3("Alg3").createBlock());
-            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents4("Alg4").createBlock());
+            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents3("Alg1").createBlock());
+            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents4("Alg2").createBlock());
+            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents1("Alg3").createBlock());
+            this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockComponents2("Alg4").createBlock());
             this.addBlockToEvaluation(new VeinBiometricSystem.VeinProcessingBlockFused());
         }
 
